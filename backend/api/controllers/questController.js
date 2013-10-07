e49070b7f60a2c66e7579bd31dc0062ccbf23dc1
JeanTahra <vesa.lahdenpera@tut.fi>
2013-10-07 23:01:54 +0300

    Created controllers for user register and quest completion.

--- backend/api/controllers/questController.js
+++ backend/api/controllers/questController.js
@@ -5,6 +5,12 @@
  * @description	:: Contains logic for handling requests.
  */
 
+
+//Tekee questin userille
+//json-objekti requestiin mallia:
+//{"msg":"base64(Username:deviceIDMD5Hash:distance(m):hardmode)}
+//distance siis metreinä ja hardmode joko 0 tai 1 riippuen onko päällä vai ei
+//oletuksena 1 on päällä.
 module.exports = {
 
   /* e.g.
@@ -12,6 +18,31 @@ module.exports = {
     res.send('hello world!');
   }
   */
-  
+  	complete: function(req, res) {
+  		var temp = new Buffer(req.param("msg"), 'base64').toString('ascii');
+  		console.log(temp);
+  		temp = temp.split(":");
+
+  		user.findOne().where({name:temp[0]}).where({devid: temp[1]}).exec(function(err, u) {
+  			if(err) {
+  				return res.send(500);
+  			}
+  			console.log(u);
+  			if(u.length != 0) {
+  				quest.create({userid: u.id, distance:temp[2], hardmode:temp[3]}).done(function(err, c) {
+  					if(err) {
+  						console.log(err);
+  						return res.send(500);
+  					} else {
+  						console.log("Quest created: ", c);
+  						return res.send(200);
+  					}
+  				})
+  			} else {
+  				return res.send(403);
+  			}
+  		});
+
+  	}
 
 };
